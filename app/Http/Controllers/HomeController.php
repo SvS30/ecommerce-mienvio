18297117f6fd183b6b738b1f6081c807f832da02
salimv <salimvzqz@gmail.com>
2023-02-26 15:13:11 -0600

    Feature: Products filtered by name and filters on HomeController with return home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,11 +18,20 @@ class HomeController extends Controller
 
     /**
      * Show the application dashboard.
+     * with Product list and filters
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $query = \App\Models\Product::orderBy('nombre', 'ASC');
+        $product_keyword = $request->product_name;
+        $product_price_min = $request->price_range_min;
+        $product_price_max = $request->price_range_max;
+        if ($product_keyword) $query = $query->where('nombre', 'LIKE', '%'. $product_keyword .'%');
+        if ($product_price_min) $query = $query->where('precio', '>=', $product_price_min);
+        if ($product_price_max) $query = $query->where('precio', '<=', $product_price_max);
+        $products = $query->paginate(15);
+        return view('home', compact('products', 'product_keyword', 'product_price_min', 'product_price_max'));
     }
 }
